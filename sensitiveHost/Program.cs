940697a8c9cc66ebd4f807b63e606f18ed9750d5
Jakob Stengard <jakob.stengard@omegapoint.se>
2023-08-18 09:40:03 +0200

    Fixed spelling mistake

--- sensitiveHost/Program.cs
+++ sensitiveHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "I contain sensitive information including credentials. If you can read this them the attack has been successful.");
+app.MapGet("/", () => "I contain sensitive information including credentials. If you can read this then the attack has been successful.");
 
 app.Run();
